3f25f54808ecc9f4188d589d28bd26f1ed61ab8b
jclaw <jclaw3456@gmail.com>
2016-06-03 17:59:44 -0700

    working raise and lower

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+	var matches = [];
+
+	$('#raiseHierarchy').click(function() {
+		raiseHierarchy();
+	})
+
+	$('#lowerHierarchy').click(function() {
+		lowerHierarchy();
+	})
+
+	function raiseHierarchy() {
+		modifyHeaders('raise');
+	}
+
+	function lowerHierarchy() {
+		modifyHeaders('lower');
+	}
+
+	function modifyHeaders(operation) {
+		var input = $('#wikiText').val();
+		var inputLines = input.split('\n');
+		var output = '';
+		// if (matches.length == 0) {
+		// 	matches = Array(inputLines.length).fill(0);
+		// }
+		// console.log(matches);
+
+		for (var i = 0; i < inputLines.length; i++) {
+			var line = inputLines[i];
+			var numEqualChars = 0;
+			var header;
+			var regex = new RegExp('(^=+)([^=]*)(=+$)');
+			var alerted = false;
+			var n = line.replace(regex, function(match, p1, p2, p3, offset, string) {
+				if (p1 === p3) {
+					// matches[i] = 1;
+					if (operation == 'raise')
+						// if (!alerted && p1.length == 1) alert('')
+						return [p1.slice(0,-1), p2, p3.slice(0,-1)].join('');
+					else if (operation == 'lower')
+						return [p1 + '=', p2, p3 + '='].join('');
+				} else {
+					return string;
+				}
+			});
+			output += n;
+			if (i < inputLines.length - 1) output += '\n';
+		};
+		$('#wikiText').val(output);
+	}
+})
